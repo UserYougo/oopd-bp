1e6f1f9a34fbe92655d1101f1d27fdaae59232c7
UserYougo <hugo@poelen.nl>
2025-10-09 15:41:09 +0200

    Update

--- src/main/java/nl/han/student/HJMPoelen/App.java
+++ src/main/java/nl/han/student/HJMPoelen/App.java
@@ -2,7 +2,7 @@ package nl.han.student.HJMPoelen;
 
 import com.github.hanyaeger.api.YaegerGame;
 
-
+//Deze hier is het
 public class App extends YaegerGame{
     public static void main(String[] args) {
         YaegerGame.launch(args);
